68096057f6fef60ca89965fce9da9153532166ae
enghonorios <154471089+enghonorios@users.noreply.github.com>
2023-12-23 18:36:16 -0300

    Python Tabuada simples

---
+++ ex_tabuada simples.py
@@ -0,0 +1,29 @@
+faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada
+
+n1= float(input('Digite um número para ver sua tabuada: '))
+resultado = n1
+
+print(f'{n1:.0f}x1= {n1*1:.0f}')
+print(f'{n1:.0f}x2= {n1*2:.0f}')
+print(f'{n1:.0f}x3= {n1*3:.0f}')
+print(f'{n1:.0f}x4= {n1*4:.0f}')
+print(f'{n1:.0f}x5= {n1*5:.0f}')
+print(f'{n1:.0f}x6= {n1*6:.0f}')
+print(f'{n1:.0f}x7= {n1*7:.0f}')
+print(f'{n1:.0f}x8= {n1*8:.0f}')
+print(f'{n1:.0f}x9= {n1*9:.0f}')
+print(f'{n1:.0f}x10= {n1*10:.0f}')
+
+Digite um número para ver sua tabuada: 5
+5x1= 5
+5x2= 10
+5x3= 15
+5x4= 20
+5x5= 25
+5x6= 30
+5x7= 35
+5x8= 40
+5x9= 45
+5x10= 50
+
+Process finished with exit code 0
